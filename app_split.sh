9856265b82c2efc681df36e19f6d4931520a4bae
Chenmin <cnlll@qq.com>
2021-08-26 17:41:03 +0800

    war 变  tar.gz

--- app_split.sh
+++ app_split.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+unzip app/bps.war -d app/bps
+unzip app/workspace.war -d app/workspace
+unzip app/governor.war -d app/governor
+cd app/bps
+tar zcvf ../bps.tar.gz .
+cd ../../app/workspace
+tar zcvf ../workspace.tar.gz .
+cd ../../app/governor
+tar zcvf ../governor.tar.gz .
+cd ../../
 cd app
 split -b 10m bps.tar.gz bps.s
 split -b 10m workspace.tar.gz workspace.s
